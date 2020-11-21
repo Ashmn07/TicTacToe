4e8afa40395e9e51f4f8b2de29b2a0a39206b531
Ashwin Menon <ashwin.menon202@gmail.com>
2020-11-21 19:57:12 +0530

    Improved the square board and basic onClick enabled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css';
 import Square from './Square'
 
+
 function App() {
+  const [squareValues,setSquareValues] = useState([null,null,null,null,null,null,null,null,null])
+
+  const checkAnswer = () => {
+    const answerOptions = [
+      [0,1,2],
+      [0,3,6],
+      [0,4,8],
+      [1,4,7],
+      [2,5,8],
+      [2,4,6],
+      [3,4,5],
+      [6,7,8]
+    ]
+    answerOptions.map((answer)=>{
+      const [a,b,c] = answer;
+      if(squareValues[a] && squareValues[a]===squareValues[b] && squareValues[a]===squareValues[c])
+        return squareValues[a];
+      else
+      return null;
+    })
+  }
+
+  function handleClick(id){
+    console.log(id)
+    console.log(`Element ${id} has been clicked`)
+  }
+
   return (
     <div className="App">
-      <Square/>
+      <h1>Welcome to TIC-TAC-TOE</h1>
+      <div className="square">
+        {squareValues.map((squareValue,index)=>(
+          <Square key={index} id={index} squareValue={squareValue} onClick={()=> handleClick(index)}/>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Square.js
+++ src/Square.js
@@ -1,25 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Square.css'
 
-function Square() {
+
+function Square({squareValue,onClick,id}) {
     return (
-         <div className="square">
-            <div className="square__row">
-                <button className="square__cell"></button>
-                <button className="square__cell"></button>
-                <button className="square__cell"></button>
-            </div>
-            <div className="square__row">
-                <button className="square__cell"></button>
-                <button className="square__cell"></button>
-                <button className="square__cell"></button>
-            </div>
-            <div className="square__row">
-                <button className="square__cell"></button>
-                <button className="square__cell"></button>
-                <button className="square__cell"></button>
-            </div>
-        </div>
+        <>
+        {console.log(id)}
+            <button value={squareValue} onClick={onClick} className="square__cell"></button>
+            {/* <button value={squareValues[1]} className="square__cell"></button>
+            <button value={squareValues[2]} className="square__cell"></button>
+            <button value={squareValues[3]} className="square__cell"></button>
+            <button value={squareValues[4]} className="square__cell"></button>
+            <button value={squareValues[5]} className="square__cell"></button>
+            <button value={squareValues[6]} className="square__cell"></button>
+            <button value={squareValues[7]} className="square__cell"></button>
+            <button value={squareValues[8]} className="square__cell"></button> */}
+        </>
     )
 }
 
